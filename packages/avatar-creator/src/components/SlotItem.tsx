45031e233f98619e0ee5f38ea72a8d61aa598ba9
Dom Taylor <dom@msquared.io>
2025-10-14 17:39:08 +0100

    feat: coloring

--- packages/avatar-creator/src/components/SlotItem.tsx
+++ packages/avatar-creator/src/components/SlotItem.tsx
@@ -10,7 +10,7 @@ import * as React from "react";
 import { useEffect, useState } from "react";
 
 import { AvatarLoader } from "../scripts/avatar-loader";
-import { CatalogBodyTypeKey, CatalogPartKey } from "../types/Catalog";
+import { CatalogPartKey } from "../types/Catalog";
 import styles from "./SlotItem.module.css";
 
 // Extend Window interface to include avatarLoader
@@ -21,7 +21,6 @@ declare global {
 }
 
 export default function SlotItem({
-  bodyType,
   skin,
   slot,
   modelUrl,
@@ -30,7 +29,6 @@ export default function SlotItem({
   avatarLoader,
   onClick,
 }: {
-  bodyType?: CatalogBodyTypeKey;
   skin?: string;
   slot?: CatalogPartKey;
   // No secondary needed here as loading events entirely predicated on the primary model.
@@ -71,13 +69,37 @@ export default function SlotItem({
   }, [slot, modelUrl]);
 
   return (
-    <li
+    <svg
       className={`${styles.slotItem} ${active ? styles.active : ""} ${loading ? styles.loading : ""}`}
-      data-bodytype={bodyType ?? null}
-      data-skin={skin ?? null}
       onClick={onClick}
+      width="98"
+      height="98"
+      viewBox="-2 -2 102 102"
+      fill="none"
+      xmlns="http://www.w3.org/2000/svg"
     >
-      <img src={thumbnailUrl} draggable="false" />
-    </li>
+      <defs>
+        <clipPath id="slot-clip">
+          <path d="M5.44056 97.5C2.70694 97.5 0.5 95.2844 0.5 92.56V19.94C0.5 17.7625 1.36746 15.6697 2.9038 14.1336L14.1349 2.90357C15.6713 1.36739 17.7643 0.5 19.942 0.5H92.5594C95.2931 0.5 97.5 2.71559 97.5 5.44V78.05C97.5 80.2275 96.6325 82.3203 95.0962 83.8564L83.8651 95.0864C82.3287 96.6226 80.2357 97.49 78.058 97.49L5.44056 97.5Z" />
+        </clipPath>
+      </defs>
+
+      <path
+        d="M5.44056 97.5C2.70694 97.5 0.5 95.2844 0.5 92.56V19.94C0.5 17.7625 1.36746 15.6697 2.9038 14.1336L14.1349 2.90357C15.6713 1.36739 17.7643 0.5 19.942 0.5H92.5594C95.2931 0.5 97.5 2.71559 97.5 5.44V78.05C97.5 80.2275 96.6325 82.3203 95.0962 83.8564L83.8651 95.0864C82.3287 96.6226 80.2357 97.49 78.058 97.49L5.44056 97.5Z"
+        strokeWidth="4"
+        stroke="inherit"
+        fill="inherit"
+      />
+
+      <image
+        href={thumbnailUrl}
+        x="6"
+        y="6"
+        width="86"
+        height="86"
+        clipPath="url(#slot-clip)"
+        className={`${styles.slotItemImage} ${skin ? styles.slotSkinMask : ""}`}
+      />
+    </svg>
   );
 }
